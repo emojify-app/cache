64dbd70adf159e734fb5f8db416acdb72f3e79d9
Nic Jackson <jackson.nic@gmail.com>
2019-02-09 09:17:30 +0000

    Addd mock for cache client

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	github.com/go-redis/redis v6.15.1+incompatible
 	github.com/golang/protobuf v1.2.0
 	github.com/hashicorp/go-hclog v0.0.0-20190109152822-4783caec6f2e
+	github.com/keegancsmith/rpc v1.1.0 // indirect
 	github.com/nicholasjackson/env v0.2.0
+	github.com/stamblerre/gocode v0.0.0-20190205225106-e620fbf44f15 // indirect
 	github.com/stretchr/testify v1.3.0
 	golang.org/x/net v0.0.0-20190206173232-65e2d4e15006
 	google.golang.org/grpc v1.18.0
---
+++ protos/cache/cache_mock.go
@@ -0,0 +1,46 @@
+package cache
+
+import (
+	wrappers "github.com/golang/protobuf/ptypes/wrappers"
+	"github.com/stretchr/testify/mock"
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
+)
+
+// ClientMock is a mock implementation of the gRCP cache client
+type ClientMock struct {
+	Mock mock.Mock
+}
+
+// Put is a mock implementation of the cache put interface
+func (c *ClientMock) Put(ctx context.Context, in *CacheItem, opts ...grpc.CallOption) (*wrappers.StringValue, error) {
+	args := c.Mock.Called(ctx, in, opts)
+
+	if sv := args.Get(0); sv != nil {
+		return sv.(*wrappers.StringValue), args.Error(1)
+	}
+
+	return nil, args.Error(0)
+}
+
+// Get is a mock implementation of the cache get interface
+func (c *ClientMock) Get(ctx context.Context, in *wrappers.StringValue, opts ...grpc.CallOption) (*CacheItem, error) {
+	args := c.Mock.Called(ctx, in, opts)
+
+	if sv := args.Get(0); sv != nil {
+		return sv.(*CacheItem), args.Error(1)
+	}
+
+	return nil, args.Error(0)
+}
+
+// Exists is a mock implementation of the cache exists interface
+func (c *ClientMock) Exists(ctx context.Context, in *wrappers.StringValue, opts ...grpc.CallOption) (*wrappers.BoolValue, error) {
+	args := c.Mock.Called(ctx, in, opts)
+
+	if sv := args.Get(0); sv != nil {
+		return sv.(*wrappers.BoolValue), args.Error(1)
+	}
+
+	return nil, args.Error(0)
+}
